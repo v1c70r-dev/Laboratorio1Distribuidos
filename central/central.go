43e9edbd97f301b1d74a7b310be68e17425cf153
Victor <sproutvmc@gmail.com>
2022-09-10 02:38:12 -0300

    Se cean servicios sincronos y asincronos

--- central/central.go
+++ central/central.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"time"
+
+	pb "Laboratorio1Distribuidos/proto"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+	"google.golang.org/grpc"
 )
 
 func failOnError(err error, msg string) {
@@ -13,7 +19,11 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	helpQueue := "SOS"                                              //Nombre de la cola
+	hostQ := "localhost"                                            //Host de RabbitMQ 172.17.0.1
+	hostS := "localhost"                                            //Host de un Laboratorio
+	conn, err := amqp.Dial("amqp://guest:guest@" + hostQ + ":5672") //Conexion con RabbitMQ
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -22,16 +32,16 @@ func main() {
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		helpQueue, // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
-	msgs, err := ch.Consume(
+	chDelivery, err := ch.Consume(
 		q.Name, // queue
 		"",     // consumer
 		true,   // auto-ack
@@ -44,11 +54,41 @@ func main() {
 
 	var forever chan struct{}
 
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
+	for delivery := range chDelivery {
+		port := ":50051"                                           //puerto de la conexion con el laboratorio
+		fmt.Println("Pedido de ayuda de " + string(delivery.Body)) //obtiene el primer mensaje de la cola
+		connS, err := grpc.Dial(hostS+port, grpc.WithInsecure())   //crea la conexion sincrona con el laboratorio
+
+		if err != nil {
+			panic("No se pudo conectar con el servidor" + err.Error())
 		}
-	}()
+
+		serviceCliente := pb.NewMessageServiceClient(connS)
+
+		for {
+			//envia el mensaje al laboratorio
+			res, err := serviceCliente.Intercambio(context.Background(),
+				&pb.Message{
+					Body: "Equipo listo?",
+				})
+
+			if err != nil {
+				panic("No se puede crear el mensaje " + err.Error())
+			}
+
+			fmt.Println(res.Body)       //respuesta del laboratorio
+			time.Sleep(5 * time.Second) //espera de 5 segundos
+			defer connS.Close()
+		}
+	}
+
+	// go func() {
+	// 	for d := range msgs {
+	// 		log.Printf("Received a message: %s", d.Body)
+	// 		//fmt.Println("Queue content", q)
+
+	// 	}
+	// }()
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
--- lab_renca/labRenca.go
+++ lab_renca/labRenca.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	pb "Laboratorio1Distribuidos/proto"
 	"context"
+	"fmt"
 	"log"
+	"math/rand"
+	"net"
 	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+	"google.golang.org/grpc"
 )
 
 func failOnError(err error, msg string) {
@@ -14,8 +19,23 @@ func failOnError(err error, msg string) {
 	}
 }
 
+// Permite coenxión cola síncrona proto
+type server struct {
+	pb.UnimplementedMessageServiceServer
+}
+
+func (s *server) Intercambio(ctx context.Context, msg *pb.Message) (*pb.Message, error) {
+	fmt.Println(msg.Body)
+	return &pb.Message{Body: "NO"}, nil
+}
+
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	labName := "labRenca"                                           //nombre del laboratorio
+	helpQueue := "SOS"                                              //nombre de la cola
+	hostQ := "localhost"                                            //ip del servidor de RabbitMQ 172.17.0.1
+	conn, err := amqp.Dial("amqp://guest:guest@" + hostQ + ":5672") //conexion con RabbitMQ
+
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -24,18 +44,19 @@ func main() {
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		helpQueue, // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	body := "Hello World!"
+	//Mensaje enviado a la cola de RabbitMQ (Llamado de emergencia)
+	body := msgEstallido(labName)
 	err = ch.PublishWithContext(ctx,
 		"",     // exchange
 		q.Name, // routing key
@@ -47,4 +68,56 @@ func main() {
 		})
 	failOnError(err, "Failed to publish a message")
 	log.Printf(" [x] Sent %s\n", body)
+
+	//Conexión cola síncrona (proto)
+	listener, err := net.Listen("tcp", ":50051") //conexion sincrona
+	if err != nil {
+		panic("La conexion no se pudo crear" + err.Error())
+	}
+
+	serv := grpc.NewServer()
+	for {
+		pb.RegisterMessageServiceServer(serv, &server{})
+		if err = serv.Serve(listener); err != nil {
+			panic("El server no se pudo iniciar" + err.Error())
+		}
+	}
+}
+
+/*
+Retorna el nombre del lab si ocurre un estallido
+*/
+func msgEstallido(lab_name string) string {
+	/*Cada 5 segundos, con una probabilidad de 0.8 se verá la ocurrencia de un estallido
+	(0.8 ocurre estallido / 0.2 no ocurre estallido)*/
+	var estadoEstallido bool
+	c := time.Tick(5 * time.Second)
+
+	for range c {
+		estadoEstallido = statusUpdateEstallido()
+		if estadoEstallido {
+			fmt.Println("SOS Enviado a Central. Esperando respuesta...")
+			return lab_name
+		}
+	}
+	return "false"
+}
+
+func statusUpdateEstallido() bool {
+	//Se generan números equiprobables del 1 al 5. Números del 1 al 4 => estallido
+	// y 5 => no estallido. Esto da 4/5 (o 0.8) de pbb para estallido y 0.2 de pbb para no estallido.
+	estallido := false
+	max := 6
+	min := 1
+	random := rand.Intn(max-min) + min
+
+	if random < 5 {
+		estallido = true //true
+		fmt.Println("Analizando estado Laboratorio: [ ESTALLIDO ]")
+	} else {
+		estallido = false //false
+		fmt.Println("Analizando estado Laboratorio: [ OK ]")
+	}
+
+	return estallido
 }
